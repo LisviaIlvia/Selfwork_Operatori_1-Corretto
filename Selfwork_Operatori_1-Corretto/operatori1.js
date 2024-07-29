8f6a8b34fd8b42d39cf62cd8918061657b3858c6
LisviaIlvia <silviaercolino@yahoo.it>
2024-07-29 15:46:54 +0200

    Corretto xD

---
+++ Selfwork_Operatori_1-Corretto/operatori1.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 93;
+let c = 8;
+let d = 532;
+let e = 59;
+
+let somma = a + b + c + d + e;
+
+let media = (somma) / 5;
+
+alert(`La somma tra i numeri equivale a ${somma} mentre la media equivale a ${media}`);
+
